140933767c5c799956e804c62f5f07abc4ec692e
jeevideos <jeevideos51@gmail.com>
2025-06-28 20:35:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4790,8 +4790,8 @@ def product_fullscreen(product_id):
 
 # Add this after imports (before routes)
 ADMIN_CREDENTIALS = {
-    "username": "cdbrathor@gmail.com",  # CHANGE THIS
-    "password": "@@@@7691909051m"      # CHANGE THIS
+    "username": "cronyzo_admin",  # CHANGE THIS
+    "password": "Admin@1234"      # CHANGE THIS
 }
 
 admin_click_count = 0
